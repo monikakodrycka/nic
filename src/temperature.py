ca40ddd857f654e4978ba3c0060a34c5b1a1a18a
Monika <monika.kodrycka@gmail.com>
2015-06-23 15:50:17 +0200

    fixed temp  convert in f_to_k

--- src/temperature.py
+++ src/temperature.py
@@ -3,8 +3,8 @@
 """
 
 def f_to_k(temp):
-    converted = ((temp - 32) * (5 / 9)) + 273.15
+    converted = ((temp - 32) * (5 / 9.0)) + 273.15
     return converted 
 
 print(f_to_k(32))
-print(f_to_k(0))
+print(f_to_k(212))
